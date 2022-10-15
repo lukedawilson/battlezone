4d4e069d8b7962649bb01a0b1eb21dccd3d3e0c7
Luke Wilson <luke.wilson@intelligenthack.com>
2022-10-15 13:32:37 +0100

    Var to const

--- src/js/game.js
+++ src/js/game.js
@@ -17,28 +17,28 @@ class Game {
     this.renderer.setSize(width, height);
     document.body.appendChild(this.renderer.domElement);
 
-    /*var material = new THREE.LineBasicMaterial({ color: this.LIGHT_GREEN });
-    var geometry = new THREE.Geometry();
+    /*const material = new THREE.LineBasicMaterial({ color: this.LIGHT_GREEN });
+    const geometry = new THREE.Geometry();
     geometry.vertices.push(new THREE.Vector3(-2, 0, -3));
     geometry.vertices.push(new THREE.Vector3(0, 2, -3));
     geometry.vertices.push(new THREE.Vector3(2, 0, -3));
-    var line = new THREE.Line(geometry, material);
+    const line = new THREE.Line(geometry, material);
     this.camera.add(line);*/
 
-    var self = this;
+    const self = this;
     document.onkeydown = e => self._handleInput(self, e);
   }
 
   addCube(x, z) {
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var cube = this._buildShape(geometry, x, z);
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const cube = this._buildShape(geometry, x, z);
     cube.rotation.y += 2;
     this.scene.add(cube);
     return cube;
   }
 
   addPyramid(x, z) {
-    var geometry = new THREE.Geometry();
+    const geometry = new THREE.Geometry();
     geometry.vertices.push(
       new THREE.Vector3(0, 0, 0),
       new THREE.Vector3(0, 0, 1),
@@ -53,7 +53,7 @@ class Game {
       new THREE.Face3(3, 4, 2),
       new THREE.Face3(0, 4, 3));
 
-    var pyramid = this._buildShape(geometry, x, z);
+    const pyramid = this._buildShape(geometry, x, z);
     pyramid.position.y = -0.5;
     this.scene.add(pyramid);
     return pyramid;
@@ -71,34 +71,34 @@ class Game {
   }
 
   _buildShape(geometry, x, z) {
-    var edges = new THREE.EdgesGeometry(geometry);
-    var material = new THREE.LineBasicMaterial({color: this.LIGHT_GREEN});
-    var shape = new THREE.LineSegments(edges, material);
+    const edges = new THREE.EdgesGeometry(geometry);
+    const material = new THREE.LineBasicMaterial({color: this.LIGHT_GREEN});
+    const shape = new THREE.LineSegments(edges, material);
     shape.position.x = x;
     shape.position.z = z;
     return shape;
   }
 
   _getPosVector(d, theta) {
-    var hyp = d;
-    var opp = hyp * Math.sin(theta);
-    var adj = hyp * Math.cos(theta);
+    const hyp = d;
+    const opp = hyp * Math.sin(theta);
+    const adj = hyp * Math.cos(theta);
     return new THREE.Vector2(opp, adj);
   }
 
   _handleInput(sender, e) {
-    var theta = sender.camera.rotation.y - Math.PI;
-    var pos = sender._getPosVector(.1, theta);
+    const theta = sender.camera.rotation.y - Math.PI;
+    const pos = sender._getPosVector(.1, theta);
 
     if (sender.counter % 10 == 0) {
       sender.counter = 1;
 
-      var theta2 = Math.random() * Math.PI * 2 + Math.PI;
-      var pos2 = sender._getPosVector(10, theta + theta2);
-      var x2 = sender.camera.position.x + pos2.x;
-      var y2 = sender.camera.position.z + pos2.y;
+      const theta2 = Math.random() * Math.PI * 2 + Math.PI;
+      const pos2 = sender._getPosVector(10, theta + theta2);
+      const x2 = sender.camera.position.x + pos2.x;
+      const y2 = sender.camera.position.z + pos2.y;
 
-      var shape = Math.random();
+      const shape = Math.random();
       if (shape >= 0.5)
         sender.addCube(x2, y2);
       else
